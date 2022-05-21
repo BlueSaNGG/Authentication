b3fc11ebd061a161841ee3161fe1663793371ecd
BlueSaNGG <747182080@qq.com>
2022-05-21 16:45:44 -0500

    Passport with session authentication & cookies

--- Authentication/app.js
+++ Authentication/app.js
@@ -4,11 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// const encrypt = require("mongoose-encryption")
-// const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const { hash } = require("bcrypt");
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -17,6 +15,19 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+
+/////////////////////////////////////////////////
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize()); // initialize passport
+app.use(passport.session());    // use passport to manipulate sessions
+/////////////////////////////////////////////////
+
 
 // connect to mongodb
 mongoose.connect("mongodb://localhost:27017/userDB");
@@ -30,14 +41,23 @@ const userSchema = new mongoose.Schema (
     { collection: "userinfo"}
 );
 
-// plugin the encryption -> before creating the model
-
-// userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password'] });
-
-
+/////////////////////////////////////////////////
+// hash & salt password -> save user into mongodb database
+userSchema.plugin(passportLocalMongoose);
+/////////////////////////////////////////////////
 
 // create model
 const User = new mongoose.model("User", userSchema);
+
+
+/////////////////////////////////////////////////
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+/////////////////////////////////////////////////
+
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -51,55 +71,49 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 
+// enable directly visit secret url when user logged in
+app.get("/secrets", function (req, res) {
+    // check if the user is authenticated
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+
+});
+
+app.get("/logout", function (req, res) {
+    req.logout(function () { console.log('Done logging out.'); });
+    res.redirect("/");
+    
+});
+
 // catch the post from the register route
 app.post("/register", function (req, res) {
 
-    // hash&salt
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        // from the input name to get the input 
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-
-        newUser.save(function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                // only render after user registered
-                res.render("secrets");
-            }
-        });
-    });
-
-
+    // from the passport-local-mongoose package
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local") (req, res, function () {
+                res.redirect("/secrets");
+            });
+        }
+    }); 
 
 });
 
 // catche the post from the login route
-app.post("/login", function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email: username}, function (err, foundUser) {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                // hash the password
-               bcrypt.compare(password, foundUser.password, function(err, result) {
-                   if (result === true) {
-                       res.render("secrets");
-                   }
-               });
-            } else {
-                console.log("Username not found!")
-            }
-        }
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/secrets');
     });
-});
+
+
 
 app.listen(3000, function() {
     console.log("Server started on port 3000.")
-})
+});

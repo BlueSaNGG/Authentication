37f72057ba6a45a4bec7ac13fd957dd55e2928f7
BlueSaNGG <747182080@qq.com>
2022-05-21 14:22:51 -0500

    Hash & Salt

--- Authentication/app.js
+++ Authentication/app.js
@@ -5,7 +5,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption")
-const md5 = require("md5");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const { hash } = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -50,38 +53,51 @@ app.get("/register", function (req, res) {
 
 // catch the post from the register route
 app.post("/register", function (req, res) {
-    // from the input name to get the input 
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    // hash&salt
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        // from the input name to get the input 
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                // only render after user registered
+                res.render("secrets");
+            }
+        });
     });
 
-    newUser.save(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            // only render after user registered
-            res.render("secrets");
-        }
-    });
+
+
 });
 
 // catche the post from the login route
 app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}, function (err, foundUser) {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
+                // hash the password
+               bcrypt.compare(password, foundUser.password, function(err, result) {
+                   if (result === true) {
+                       res.render("secrets");
+                   }
+               });
+            } else {
+                console.log("Username not found!")
             }
         }
-    })
+    });
 });
 
 app.listen(3000, function() {
